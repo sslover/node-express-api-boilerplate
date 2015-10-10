3f025a434fa5d3cf8284839b4cd293822406eb7d
sslover <samjslover@gmail.com>
2015-10-10 01:32:13 -0400

    updating readme

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,21 @@ router.get('/', function(req, res, next) {
   
   var jsonData = {
   	'name': 'node-express-api-boilerplate',
-  	'api-status':'OK'
+  	'api-status':'hello'
   }
 
   // respond with json data
   res.json(jsonData)
 });
 
+router.get('/api/v1/test', function(req,res,next){
+	  var jsonData = {
+  	'name': 'node-express-api-boilerplate',
+  	'api-status':'msdfsfs'
+  }
 
+  // respond with json data
+  res.json(jsonData)
+})
 
 module.exports = router;
